cceff6798ec313d68a4791421941fc0ca29cddb3
animesh2cool <63170497+animesh2cool@users.noreply.github.com>
2022-09-02 14:02:23 +0530

    Q2

---
+++ 30_08_22 Assignment/MyProgram1.java
@@ -0,0 +1,6 @@
+public class MyProgram1 {
+	public static void main(String args[]) {
+		DisplayMessage program = new DisplayMessage();  //object creation by the class DisplayMessage
+		program.printMessage(); 	
+	}
+}
