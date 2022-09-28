f9623642b73f7f9271b9ffbf58ce3a21b6a1dc9a
animesh2cool <63170497+animesh2cool@users.noreply.github.com>
2022-09-28 14:43:41 +0530

    Q2

---
+++ 27_09_22 Assignment/ArrayColour.java
@@ -0,0 +1,14 @@
+//2. Write a Java program to create a new array list, 
+//add some colors (string) and print out the collection.
+
+package array;
+
+public class ArrayColour {
+	public static void main(String[] args) {
+		
+	 String [] color = new String [] {"red","yellow","blue","black"} ;  //storing colours in Array
+	 for(String i : color) {
+			System.out.println(i );
+          }
+	 }
+}
